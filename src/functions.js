f8c026de40edfd57ba37ff833fe965674b56f99b
Giulia Carlini <giuliacarlini@gmail.com>
2020-07-12 20:47:30 -0300

    Adicionado Promise para a requisão da API do Twitter

--- src/functions.js
+++ src/functions.js
@@ -3,32 +3,46 @@ var fs = require('fs');
 var dateFormat = require('dateformat');
 
 module.exports = {
-    DownloadTweet(tweet, caminho) {
+    DownloadTweet(tweet, caminho, ehLocal) {
         if (tweet) {
             if (tweet.hasOwnProperty('extended_entities')) {
+                
                 if ((tweet.is_quote_status == false) && (tweet.extended_entities != null)) {   
                     for (var y = 0; y < tweet.extended_entities.media.length; y++) {
                         var media = tweet.extended_entities.media[y];
-                        if (media.video_info != null) {                  
+                        if (media.video_info != null) { 
+
+                            var object = {}; 
+                            var key = "media";
+                            object[key] = [];
+
                             for (var i = 0; i < media.video_info.variants.length; i++) {   
-                                var variants = media.video_info.variants[i];      
-                                
-                                var extensao = retornaExtensaoArquivo(variants);
-                
-                                if (extensao) {
-                                    const file = fs.createWriteStream(caminho+dateFormat(new Date(), "yyyymmddHHMMss")+"-"+tweet.user.screen_name+
-                                                                                                "-"+tweet.id_str+"-"+i+extensao);
-                                    
-                                    https.get(variants.url, function(response) {
-                                    response.pipe(file);                                
-                                    }); 
-                                }                              
+                                var variants = media.video_info.variants[i];                                     
+
+                                if (ehLocal) {       
+
+                                    var extensao = retornaExtensaoArquivo(variants);
+                    
+                                    if (extensao) {
+                                        const file = fs.createWriteStream(caminho+dateFormat(new Date(), "yyyymmddHHMMss")+"-"+tweet.user.screen_name+
+                                                                                                    "-"+tweet.id_str+"-"+i+extensao);
+                                        
+                                        https.get(variants.url, function(response) {
+                                        response.pipe(file);                                
+                                        }); 
+                                    }    
+                                } else {
+                                    var data = { url: variants.url };                                    
+                                    object[key].push(data);
+                                }
                             }          
                         }
                     }
                 }  
             }          
         }
+        
+        return object;
     }    
 }
 
--- src/twitterController.js
+++ src/twitterController.js
@@ -4,16 +4,27 @@ const functions = require("./functions");
 module.exports = {
   async downloadTweet(request, response) {
     const id = request.params.id;
-    var caminho = request.headers.caminho;
+    const caminho = request.headers.caminho;
 
-    T.get('statuses/show', { id: [id], trim_user: false, include_entities: true, include_ext_alt_text: true } , function(err, data, response) {     
-      if (!err) {
-        functions.DownloadTweet(data, caminho);        
-      }        
-    });   
-    
-    return response.status(200).send();    
+    const init = async() => {
+      try {
+        const tweet = await T.get('statuses/show', { id: [id], trim_user: false, include_entities: true, include_ext_alt_text: true });    
+        return tweet.data;
+      } catch(err) {
+        return err.code;
+      }
+    };
+
+    var data = await init();
+
+    if (data == 144) {
+      return response.status(401).json({error : "144 - Sem status para esse ID"});    
+    } else {
+      var urls = functions.DownloadTweet(data, caminho, false);
+      return response.status(200).json(urls);    
+    }       
   }
 }
+
 
 
